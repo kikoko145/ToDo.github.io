3bd877147b75f4084bc29c9e2fe09bd94ec1ca70
Arzeyra <91270449+kikoko145@users.noreply.github.com>
2022-07-11 17:37:56 +0700

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,15 +1,15 @@
-var listElement = document.querySelector("app ul");
-var inputElement = document.querySelector("app  input");
-var buttonElement = document.querySelector("app button");
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app  input");
+var buttonElement = document.querySelector("#app button");
 var todos = JSON.parse(localStorage.getItem("list_todos")) || [];
 function renderTodos() {
     listElement.innerHTML = "";
     for (todos of todos) {
         var todoElement = document.createElement("li");
-        var todoText = document.createTextNode(todos);
+        var todoText = document.createTextNode(todo);
         var linkElement = document.createElement("a");
         linkElement.setAttribute("href", "#");
-        var pos = todos.indexOf(todos);
+        var pos = todos.indexOf(todo);
         linkElement.setAttribute("onclick", "deleteTodo(" + pos + ")");
         var linkText = document.createTextNode("done");
         linkElement.appendChild(linkText);
@@ -23,6 +23,12 @@ function addTodo() {
     var todoText = inputElement.value;
     todos.push(todoText);
     inputElement.value = "";
+    renderTodos();
+    saveToStorage();
+}
+buttonElement.oneclick = addTodo;
+function deleteTodo(pos) {
+    todo.splice(pos, 1);
     renderTodos();
     saveToStorage();
 }
